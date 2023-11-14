ccca2e5e226f99208b724d254b8fcfef951914c2
Levon Hovhannisyan <lehovhan@microsoft.com>
2023-11-14 19:12:35 +0400

    Add project files.

---
+++ CollectionsTest/Program.cs
@@ -0,0 +1,50 @@
+﻿namespace CollectionsTest
+{
+	class Person
+	{
+		public int Id { get; set; }
+		public string? Name { get; set; }
+
+		public override bool Equals(object? obj)
+		{
+			Person? p = obj as Person;
+
+			if (p != null)
+			{
+				return Id == p.Id && Name == p.Name;
+			}
+			return false;
+		}
+
+		public override int GetHashCode()
+		{
+			return Id.GetHashCode() ^ (Name?.GetHashCode() ?? 0);  // XOR
+		}
+	}
+
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			// create HashSet of Persons with initial values
+			var persons = new HashSet<Person>
+			{
+				new Person { Id = 1, Name = "Joe" },
+				new Person { Id = 2, Name = "Mary" },
+				new Person { Id = 3, Name = "Sue" }
+			};
+
+			Person person = new Person() { Id = 3, Name = "Sue" };
+
+			// check if HashSet contains person
+			if (persons.Contains(person))
+			{
+				System.Console.WriteLine("Person found");
+			}
+			else
+			{
+				System.Console.WriteLine("Person not found");
+			}
+		}
+	}
+}
